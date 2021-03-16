e78a0362db0efdd44051dfb9cce3a03d645977d6
webbdm <gwebb762@gmail.com>
2021-03-15 22:44:47 -0500

    VOD-1 added favorites GetAll

---
+++ webbdm_verse_of_the_day/Controllers/FavoritesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using webbdm_verse_of_the_day.Models;
+
+namespace webbdm_verse_of_the_day.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FavoritesController : ControllerBase
+
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FavoritesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllFavorites()
+        {
+            return Ok(_appDbContext.Favorites);
+        }
+
+
+
+    }
+}
