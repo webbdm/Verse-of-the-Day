575fdaabdd16666c7ee89291b4325295143f4143
webbdm <gwebb762@gmail.com>
2021-03-15 23:19:12 -0500

    VOD-2 Update Verse model properties

--- webbdm_verse_of_the_day/Models/Verse.cs
+++ webbdm_verse_of_the_day/Models/Verse.cs
@@ -8,8 +8,21 @@ namespace webbdm_verse_of_the_day.Models
     {
         [Key]
         public int Id { get; set; }
+
+        public string BibleReferenceLink { get; set; }
         public string Book { get; set; }
         public int Chapter { get; set; }
-        public int Text { get; set; }
+        public string FacebookShareUrl { get; set; }
+        public string  ImageLink { get; set; }
+        public bool IsValid { get; set; }
+        public string PinterestShareUrl { get; set; }
+        public string ReferenceLink { get; set; }
+        public string ReferenceText { get; set; }
+        public string TwitterShareUrl { get; set; }
+        public string Url { get; set; }
+        public DateTime VerseDate { get; set; }
+        public string VerseNumbers { get; set; }
+        public string VerseText { get; set; }
+        public string VideoLink { get; set; }
     }
 }
