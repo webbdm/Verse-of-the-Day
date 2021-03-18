4a5ae9410a05852df2bdbe5a0f2d689df47aa079
webbdm <gwebb762@gmail.com>
2021-03-17 23:11:13 -0500

    VOD-7 Created basic Favorite logic in VersesController

--- webbdm_verse_of_the_day/Controllers/VerseController.cs
+++ webbdm_verse_of_the_day/Controllers/VerseController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,10 +16,12 @@ namespace webbdm_verse_of_the_day.Controllers
 
     {
         private readonly IBibleService _bible;
+        private readonly AppDbContext _context;
 
-        public VersesController(IBibleService bible)
+        public VersesController(IBibleService bible, AppDbContext context)
         {
             _bible = bible;
+            _context = context;
         }
 
         private async Task<VerseResponse> GetVerses(string startDate, int pageSize)
@@ -40,6 +43,48 @@ namespace webbdm_verse_of_the_day.Controllers
             ViewBag.verses =  GetVerses(verseRequest.StartDate, verseRequest.PageSize).Result.verses;
 
             return View("VerseResults");
+        }
+
+        [HttpPost("/favorite")]
+        public bool SaveVerseToFavorites([FromForm] Verse verse) {
+
+
+            // Create a new favorite using the verse
+            Favorite fav = new Favorite { VerseId = verse.Id };
+
+            _context.Favorites.Add(fav);
+            _context.SaveChangesAsync();
+
+
+            // Find the original verse
+            Verse foundVerse = _context.Verses.Find(fav.VerseId);
+
+
+            // Create if we have't favorite yet
+            if (foundVerse == null)
+            {
+                // Mark as favorited
+                verse.HasBeenFavorited = true;
+
+                var createdVerse = _context.Verses.Add(verse);
+
+                Console.WriteLine(createdVerse);
+                Console.WriteLine("Created Verse");
+
+            }
+            else {
+
+                // Update as favorited (if not created)
+                foundVerse.HasBeenFavorited = true;
+                Console.WriteLine($"{foundVerse.Book} {foundVerse.Chapter}:{foundVerse.VerseNumbers} has been saved To Favorites");
+
+            }
+
+
+            _context.SaveChangesAsync();
+
+
+            return true;
         }
 
     }
