17130547eaad89867851001d2bfa01d9774ee14a
webbdm <gwebb762@gmail.com>
2021-03-21 19:03:14 -0500

    VOD-18 Converted NavMenu.js to a stateless component

--- webbdm_verse_of_the_day/ClientApp/src/components/Home.js
+++ webbdm_verse_of_the_day/ClientApp/src/components/Home.js
@@ -20,9 +20,6 @@ export const Home = () => {
                     pageSize: parseInt(pageSize)
                 })
         });
-
-
-
         
         setVerses(await result.json());
 
--- webbdm_verse_of_the_day/ClientApp/src/components/NavMenu.js
+++ webbdm_verse_of_the_day/ClientApp/src/components/NavMenu.js
@@ -2,36 +2,14 @@ import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
-    return (
-        <div className="navigation">
-            <div className="nav-left">
-                <img className="logo" src="../../Assets/klove_logo.png" />
-            </div>
-            <h1 className="nav-title">Verse of the Day</h1>
-            <div className="nav-right">
-                <a href="/">Get Verses</a>
-                <a href="/favorites">My Favorites</a>
-            </div>
+export const NavMenu = () =>
+    <div className="navigation">
+        <div className="nav-left">
+            <img className="logo" src="../../Assets/klove_logo.png" />
         </div>
-    );
-  }
-}
+        <h1 className="nav-title">Verse of the Day</h1>
+        <div className="nav-right">
+            <a href="/ClientApp">Get Verses</a>
+            <a href="ClientApp/favorites">My Favorites</a>
+        </div>
+    </div>;
